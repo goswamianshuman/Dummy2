f370c73c020910ce484d72c7deb40971e0ea9d2e
Anshuman Goswami <anshumangoswami07@gmail.com>
2022-07-31 14:04:41 +0530

    this is what we need

--- src/components/ErrorBoundaries.js
+++ src/components/ErrorBoundaries.js
@@ -7,15 +7,23 @@ class ErrorBoundary extends React.Component {
     // Define a state variable to track whether is an error or not
     this.state = { hasError: false };
   }
+  state = {
+    reload: false,
+  };
+
+  refreshPage = () => {
+    this.setState({ reload: true }, () => this.setState({ reload: false }));
+  };
+
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI
 
     return { hasError: true };
   }
-   componentDidCatch(error, errorInfo) {
+  componentDidCatch(error, errorInfo) {
     // You can use your own error logging service here
     console.log({ error, errorInfo });
-    window.location.reload();
+    this.refreshPage();
   }
   render() {
     // Check if the error is thrown
